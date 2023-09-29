927901235d4f7186cc31574cb4cd6242013bcb1d
kiyoharu1920 <kioharu1920@gmail.com>
2023-09-30 00:02:34 +0900

    54545

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+import styles from "./Footer.module.css";
+
+export  function Footer() {
+  return (
+    <div>
+      <div className={styles.navigation}>
+        <span>test</span>
+        <span>tttest</span>
+      </div>
+    </div>
+  );
+}
--- src/components/Headline/index.jsx
+++ src/components/Headline/index.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "./Headline.module.css";
 
-export default function Headline() {
+export  function Headline() {
   return (
     <div>
       <div className={styles.navigation}>
