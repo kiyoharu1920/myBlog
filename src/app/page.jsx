19ebabaebab452f38236898b278bd465ce3bf211
kiyoharu1920 <kioharu1920@gmail.com>
2023-09-29 23:47:57 +0900

    dsdsd

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,9 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-import {Header} from "../components/Header";
-
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Header></Header>
       <div className={styles.description}>
       <h1>hello world</h1>
       <h1>hello world</h1>
