aba1569c156a94e05f8b0d6ea834209c393d2f76
kiyoharu1920 <kioharu1920@gmail.com>
2023-09-29 23:46:23 +0900

    fds

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import { style } from "Header.module.css";
+import  styles  from "./Header.module.css";
 
 export default function Header() {
   return (
     <div>
-      <div className={style.navigation}>
+      <div className={styles.navigation}>
         <span>test</span>
         <span>tttest</span>
       </div>
