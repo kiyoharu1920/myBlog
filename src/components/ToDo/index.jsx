310645b9bea21ef330d68169ca43496af8ff9cc4
kiyoharu1920 <kiyoharu1920@gmail.com>
2023-10-19 21:03:01 +0900

    fix ToDoList

--- src/components/ToDo/index.jsx
+++ src/components/ToDo/index.jsx
@@ -12,28 +12,30 @@ function useToDo() {
   }, []);
 
   /* button */
-  const localStrageKey = "todoText";
+  const localStrageKey = "todoText"; /* localStrageに使うkey */
   const [todoList, setTodoList] = useState([]);
 
   const handleClickAdd = useCallback(() => {
+    /* フォームに入力された文字がすでにリストにあるか、未入力の場合は処理しない */
     if (todoList.includes(text) || text === "") {
       return;
     }
-    setTodoList((list) => [...list, text]); /* listに追加 */
+
+    setTodoList((prevList) => {
+      const array = [...prevList, text];
+      localStorage.setItem(localStrageKey, JSON.stringify(array));
+      return array;
+    }); /* listに追加 */
+
     setText((_) => ""); /* inputのvalueを削除 */
-    localStorage.setItem(localStrageKey, JSON.stringify(todoList));
   }, [todoList, text]);
+
   const handleKeyDown = useCallback(
     (e) => {
       if (e.key === "Enter") {
         e.preventDefault(); /* デフォルトのフォーム送信を防止 */
 
-        if (todoList.includes(text) || text === "") {
-          return;
-        }
-        setTodoList((list) => [...list, text]); /* listに追加 */
-        setText((_) => ""); /* inputのvalueを削除 */
-        localStorage.setItem(localStrageKey, JSON.stringify(todoList));
+        handleClickAdd();
       }
     },
     [todoList, text]
@@ -48,16 +50,6 @@ function useToDo() {
     setTodoList(() => JSON.parse(lsTodoText) || []);
   }, []);
 
-  const handleClickCheck = useCallback(function (e) {
-    const /** @type {number} */ index = e.target.name;
-    const /** @type {HTMLElement */ element = e.target;
-    const /** @type {Boolean} */ checked = e.target.checked;
-
-    const span = document.querySelector(`span[name="${index}"]`);
-
-    span.style.textDecoration = checked ? "line-through" : "none";
-  }, []);
-
   const handleClickDelete = useCallback(function (/** @type {Event} */ e) {
     const index = e.target.name;
 
@@ -68,6 +60,17 @@ function useToDo() {
       localStorage.setItem(localStrageKey, JSON.stringify(cloneArray));
       return cloneArray;
     });
+  }, []);
+
+  const handleClickCheck = useCallback(function (e) {
+    const /** @type {number} */ index = e.target.name;
+    const /** @type {HTMLElement */ element = e.target;
+    const /** @type {Boolean} */ checked = e.target.checked;
+
+    const span = document.querySelector(`span[name="${index}"]`);
+
+    /* チェックがtrueなら取り消し線を追加 */
+    span.style.textDecoration = checked ? "line-through" : "none";
   }, []);
 
   return {
