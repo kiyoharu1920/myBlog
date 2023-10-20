14ddfb8ec3f34c02ddebe9888c4d5656626c8afa
kiyoharu1920 <kiyoharu1920@gmail.com>
2023-10-20 16:45:22 +0900

    fic counter

--- src/components/Counter/index.jsx
+++ src/components/Counter/index.jsx
@@ -1,17 +1,33 @@
 "use client";
 
-import styles from "./page.module.css"
+import styles from "./page.module.css";
 import { useCallback, useState } from "react";
 
-export function Counter() {
+function counterSet() {
   const [count, setCount] = useState(0);
-  const handleClick = useCallback(() => setCount(count + 1), [count]);
+  const handleClickAdd = useCallback(
+    () => setCount((prev) => prev + 1),
+    [count]
+  );
+
+  const handleClickClear = useCallback(() => setCount(() => 0), [count]);
+
+  return { count, handleClickAdd, handleClickClear };
+}
+
+export function Counter() {
+  const { count, handleClickAdd, handleClickClear } = counterSet();
 
   return (
     <div className={styles.container}>
       カウンター
       <div>{count}回</div>
-      <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleClickAdd} className={styles.button}>
+        Click me!
+      </button>
+      <button onClick={handleClickClear} className={styles.button}>
+        Clear
+      </button>
     </div>
   );
 }
--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,7 +5,7 @@ export function Footer() {
     <div className={styles.right}>
       <div>
         <div>Contact</div>
-        <div>mail:kiyoharu1920@gmail.com</div>
+        <div>mail:xxxxxyyyyy@xxxxmail.com</div>
       </div>
     </div>
   );
